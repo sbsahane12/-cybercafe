b82cdd1d23c350f66b0aa3028b3b305801151f9a
sbsahane12 <sbsahane23@gmail.com>
2024-05-25 16:49:47 +0530

    first commit

--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -5,8 +5,8 @@ const upload = require("../utils/multer");
 const serviceController = require("../controllers/serviceController");
 const { isApplyValidService } = require("../middleware/applyserviceMiddleware");
 router.get("/services", asyncHandler(serviceController.getAllServices));
-router.get("services/:id", asyncHandler(serviceController.getSingleService));
-router.get("services/:id/apply", asyncHandler(serviceController.getApplyForm)); 
+router.get("/services/:id", asyncHandler(serviceController.getSingleService));
+router.get("/services/:id/apply", asyncHandler(serviceController.getApplyForm)); 
 
 router.post("services/:id", upload.array('documents'),isApplyValidService, asyncHandler(serviceController.setApplyService));
 module.exports = router;
